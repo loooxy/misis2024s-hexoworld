f02974c3c87a4e593cf468971b745a6d7c7d607f
Shulgin Maksim <68909789+MICRONAME@users.noreply.github.com>
2024-07-05 14:14:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
 #include <igl/opengl/glfw/Viewer.h>
 
+static float size = 1.0f;
+//x = v
+static float v = sqrt(3) * size;
+//y = h
+static float h = 1.5f * size;
+
 int main(int argc, char *argv[])
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    0,6,4,
-    0,2,6,
-    0,3,2,
-    0,1,3,
-    2,7,6,
-    2,3,7,
-    4,6,7,
-    4,7,5,
-    0,4,5,
-    0,5,1,
-    1,5,7,
-    1,7,3).finished();
+  // Inline mesh of a hex
+  const Eigen::MatrixXd V= (Eigen::MatrixXd(7,3)<<
+    1.0f * v, 0.5f * h, 0.0f, //centr 0 - 0
+    1.0f * v, 0.0f, 0.0f, // 1 - 1
+    1.0f * v, 1.0f * h, 0.0f, // 4 - 2
+    0.5f * v, 0.25f * h, 0.0f, // 6 - 3
+    1.5f * v, 0.25f * h, 0.0f, // 2 - 4
+    0.5f * v, 0.75f * h, 0.0f, // 5 - 5
+    1.5f * v, 0.75f * h, 0.0f // 3 - 6
+    ).finished();
+  const Eigen::MatrixXi F = (Eigen::MatrixXi(6,3)<<
+    0,1,4,
+    0,4,6,
+    0,6,2,
+    0,2,5,
+    0,5,3,
+    0,3,1
+    ).finished();
 
   // Plot the mesh
   igl::opengl::glfw::Viewer viewer;
