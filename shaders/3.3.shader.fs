6fd998a5cd66396fb8005bec517371548eedb8e6
giknes <biliktod8@mail.ru>
2024-09-21 23:40:14 +0300

    shaders for hexoworld basic

--- shaders/3.3.shader.fs
+++ shaders/3.3.shader.fs
@@ -1,12 +1,9 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TexCoord;
-
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+in vec4 Color;
 
 void main()
 {
-  FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+  FragColor = Color;
 }
