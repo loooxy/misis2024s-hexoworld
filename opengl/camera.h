8d8d7c0a678e597458f71d477f1ac2582608b8c9
kikikhakiri <egorfedunin@mail.ru>
2024-09-25 22:00:50 +0300

    adequate movement speed

--- opengl/camera.h
+++ opengl/camera.h
@@ -16,7 +16,7 @@ enum CameraMovement {
 // Defaut camera values
 const float YAW       = -90.0f;
 const float PITCH     = 0.0f;
-const float SPEED     = 2.5f;
+const float SPEED     = 20.0f;
 const float SENSIVITY = 0.1f;
 const float ZOOM      = 45.0f;
 
